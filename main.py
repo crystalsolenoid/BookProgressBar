b2b8b3acc3faa098c6c85cf9782d7829b711122b
Quinten Konyn <crystalsolenoid@gmail.com>
2014-08-19 19:29:06 -0700

    cleaned up/commented main.py and added more books
    also ignored *.pyc files

--- main.py
+++ main.py
@@ -1,24 +1,64 @@
-import tkinter #GUI
+#import libraries
+import tkinter #GUI library
 import random #we all know this one
-from book_class_pretty import *
 
+#import other files from this project
+from BookBar import *
+#i should probably make it import BookBar
+#	in a way other than "import *" because
+#	that can get messy but for now it'll be fine
 
 
 #creates the window we're gonna render stuff on
 window=tkinter.Tk()
 
-#ex={'title':[[1st chapter starts,2nd,3rd,etc.],current page]}
-# dBooks={'geoffrey of monmouth':[[53,75,107,149,170,186,212,262,285],154]}
-# dBooks=[[[0,1,13,43,67,91,113,137,161,177,199,225,253],43,'the emotional life of your BRAIN',tkinter.Canvas(window,width=windowWidth,height=nTotalHeight),tkinter.Label(window,text='the emotional life of your BRAIN')],[[0,1,34,68,111,151,192,241,294,360,395,449,498,557,602,665,697,745,784,819,863,917,976,1031,1069,1111,1147,1208,1266],68,'general chemistry',tkinter.Canvas(window,width=windowWidth,height=nTotalHeight),tkinter.Label(window,text='general chemistry')],[[53,75,107,149,170,186,212,262,285],0,'geoffrey of monmouth',tkinter.Canvas(window,width=windowWidth,height=nTotalHeight),tkinter.Label(window,text='geoffrey of monmouth')],[[0,1,59,93,201],0,'ibn fadlan and the land of darkness',tkinter.Canvas(window,width=windowWidth,height=nTotalHeight),tkinter.Label(window,text='ibn fadlan and the land of darkness')],[[0,1,35,63],0,'agricola and germany',tkinter.Canvas(window,width=windowWidth,height=nTotalHeight),tkinter.Label(window,text='agricola and germany')],[[1,9,14,25,37,68,83,89,124,145,157,171],0,'the holy greyhound',tkinter.Canvas(window,width=windowWidth,height=nTotalHeight),tkinter.Label(window,text='the holy greyhound')]]
 
+#
+#create our BookBars
+#
+
+#list to store instances of the BookBar class in
 dBooks=[]
 
-dBooks.append(BookBar(window,'the emotional life of your BRAIN',[0,1,13,43,67,91,113,137,161,177,199,225,253]))
+#how the BookBar creation works:
+#BookBar(tkinter window it's assigned to,
+#	book title(a string),
+#	list of chaper pages)
+#
+#the zeroith value in the list of chapter pages
+#	is the start of the introduction.
+#	if the introduction starts before page 0
+#	(for example at page iv), i just set it to 0.
 
-# BookBar(window,'the emotional life of your BRAIN',[0,1,13,43,67,91,113,137,161,177,199,225,253])
+#books Quin's currently reading for school
+dBooks.append(BookBar(window,
+	'the emotional life of your BRAIN',
+	[0,1,13,43,67,91,113,137,161,177,199,225,253]))
+
+dBooks.append(BookBar(window,
+	'general chemistry',
+	[0,1,34,68,111,151,192,241,294,360,395,449,498,
+	557,602,665,697,745,784,819,863,917,976,1031,1069,
+	1111,1147,1208,1266]))
+
+dBooks.append(BookBar(window,
+	'geoffrey of monmouth',
+	[53,75,107,149,170,186,212,262,285]))
+
+dBooks.append(BookBar(window,
+	'ibn fadlan and the land of darkness',
+	[0,1,59,93,201]))
+
+dBooks.append(BookBar(window,
+	'agricola and germany',
+	[0,1,35,63]))
+
+dBooks.append(BookBar(window,
+	'the holy greyhound',
+	[1,9,14,25,37,68,83,89,124,145,157,171]))
 
 #button actions
-def addBook():
+def addBook(): #addBook isn't implemented yet
 	sBookName=input('name of book: ')
 	nBookPages=int(input('number of pages: '))
 	nProgress=0
@@ -26,48 +66,24 @@ def canvasRefresh():
 	for book in dBooks:
 		book.renderBook()
 
-#render a book progress bar
-# def renderBook(book,x0,y0):
-# 	sColor=''#initializes the random color
-# 	nLastPage=book[0][len(book[0])-1]#find out how long the total bar is
-# 	nPageScale=min((windowWidth-nPadding*2)/nLastPage,1.5)
-# 	for nChapter in range(len(book[0])):#iterate through each chapter
-# 		if nChapter==0:#the introduction starts at page 0
-# 			nStart=0
-# 		else:#gets the first page at the chapter
-# 			nStart=(book[0][nChapter-1])*nPageScale
-# 		nEnd=(book[0][nChapter]-1)*nPageScale#gets the last page of the chapter
-# 		sColor=sSelectColor(sColor)#generates a random color
-# 		book[3].create_rectangle(x0+nStart,y0+nBookmarkOffset,x0+nEnd,y0+nBookmarkOffset+nBarHeight,fill=sColor)#draw that part of the bar in a random color
-# 		book[3].create_rectangle(x0+book[1]*nPageScale-1,y0,x0+book[1]*nPageScale+1,y0+nBarHeight+nBookmarkOffset*2,fill='red',outline='red')#draw the bookmark (current progress)
-
-# def sSelectColor(sOldColor):
-# 	sNewColor=random.choice(['cyan','blue','green','pink','orange','magenta','yellow'])
-# 	while sNewColor==sOldColor:
-# 		sNewColor=random.choice(['cyan','blue','green','pink','orange','magenta','yellow'])
-# 	return sNewColor
-
-
 
 #define the buttons
-buttonAddBook=tkinter.Button(window,text='Add A Book',command=addBook)
+buttonAddBook=tkinter.Button(window,text='Add A Book (not yet implemented)',command=addBook)
 buttonRefresh=tkinter.Button(window,text='Refresh Canvas',command=canvasRefresh)
 
-#define display features
+#define other display features
 # text=tkinter.Text(window, width=1, height=1)
 
-#renders buttons automatically
+#tk renders things automatically when you pack them
 buttonAddBook.pack()
-# text.pack()
-# canvas.pack()
 buttonRefresh.pack()
 for book in dBooks:
 	book.pack()
-	print("hix")
+
+#render the book bars
+canvasRefresh()
 
 #main loop
 window.mainloop()
 
-
-#keeps the window from closing instantly
-# window.mainloop()
+#(program ends when you close the tkinter window)
